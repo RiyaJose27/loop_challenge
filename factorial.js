9838d8381a51ee66774e7689a9f6ffd137e4cacb
Riya Jose Pratheeksh <rjosepratheeksh@gmail.com>
2021-07-24 21:45:51 -0500

    Javascript

---
+++ factorial.js
@@ -0,0 +1,7 @@
+var pro=1;
+
+for (var i=1; i<=12; i++)
+{
+    pro=pro*i;
+}
+console.log(pro);
---
+++ sigma.js
@@ -0,0 +1,7 @@
+var sum = 0;
+
+for( i = 0; i<=100; i++)
+{
+    sum = sum + i;
+}
+console.log(sum);
